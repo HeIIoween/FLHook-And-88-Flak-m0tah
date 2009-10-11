7e0ee32ebd4ab7d7872e731c87ed7c9358acdca3
M0tah <M0tah@62241663-6891-49f9-b8c1-0f2e53ca0faf>
2009-10-11 06:26:59 +0000

    Tagging latest 1.27 RC129

--- FLHook/tags/Latest/source/remoteclient.h
+++
@@ -1,8 +0,0 @@
-#ifndef _REMOTECLIENT_
-#define _REMOTECLIENT_
-
-extern "C" IMPORT class ChatClientInterface* GetChatClientInterface(void);
-IMPORT int __cdecl GetClientStats(struct client_stats_t *,int *);
-IMPORT int __cdecl GetNumClients(void);
-
-#endif
